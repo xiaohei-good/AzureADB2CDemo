1a71f740e835d35f07405a03abc83c8fe224a563
sally.sun <sally.sun@visionblue.onmicrosoft.com>
2023-10-09 11:45:51 +0200

    add

---
+++ WebApplication1/WebApplication1/Controllers/Task.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using System.Collections.Concurrent;
+
+namespace WebApplication1.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class TaskController : Controller
+    {
+        private static readonly ConcurrentDictionary<string, Todo> Tasks = new()
+        {
+            ["Task1"] = new Todo("Task1", "Description1"),
+            ["Task2"] = new Todo("Task2", "Description2")
+        };
+
+        [HttpGet]
+        [RequiredScope("tasks.read")]
+        public IActionResult Get()
+        {
+        return Json(Tasks.OrderBy(t => t.Key)
+         .Select(item => new {
+             title = item.Key,
+             description =
+         item.Value.Description
+         }));
+    }
+
+        [HttpPost]
+        [RequiredScope("tasks.write")]
+        public IActionResult Post()
+        {
+            var task = new Todo($"Task{Tasks.Count + 1}",
+            $"Description{Tasks.Count + 1}");
+            Tasks.TryAdd(task.Title, task);
+            return Json(Tasks.OrderBy(t => t.Key)
+            .Select(item => new {
+                title = item.Key,
+                description =
+            item.Value.Description
+            }));
+        }
+    }
+public  class Todo
+{
+    public Todo(string title, string description)
+    {
+        this.Title = title;
+        this.Description = description; 
+    }
+  
+    private string description;
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+    private string title;
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+}
+
+
---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+        builder.AllowAnyOrigin();
+    });
+});
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+// Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddMicrosoftIdentityWebApi(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options);
+},
+options => { builder.Configuration.Bind("AzureAdB2C", options); });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowAllOrigins");//允许跨域访问
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
